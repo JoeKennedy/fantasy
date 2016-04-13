9e61aaa51c1535bd3014aacfeed610bc80a4154a
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2016-04-13 17:55:35 -0400

    Add fight, cowardice, and bravery actions

--- Model/Action.hs
+++ Model/Action.hs
@@ -4,7 +4,7 @@ import Model.Types
 
 import ClassyPrelude.Yesod
 
-data Action = Appear | Kill | Defeat | Insult | Attack | Injure | Sentence | Torture | Sex | Force | Guilty | Innocent | Clear | Death | Raise | Pour | Drink | Finish
+data Action = Appear | Kill | Defeat | Insult | Fight | Attack | Injure | Bravery | Cowardice | Sentence | Torture | Sex | Force | Guilty | Innocent | Clear | Death | Raise | Pour | Drink | Finish
     deriving (Show, Read, Eq, Enum, Bounded)
 derivePersistField "Action"
 
@@ -15,77 +15,87 @@ multiCharacterActions :: [String]
 multiCharacterActions = map show $ filter isMultiCharacter allActions
 
 isMultiCharacter :: Action -> Bool
-isMultiCharacter Appear   = False
-isMultiCharacter Kill     = True
-isMultiCharacter Defeat   = True
-isMultiCharacter Insult   = True
-isMultiCharacter Attack   = True
-isMultiCharacter Injure   = True
-isMultiCharacter Sentence = True
-isMultiCharacter Torture  = True
-isMultiCharacter Sex      = True
-isMultiCharacter Force    = True
-isMultiCharacter Guilty   = False
-isMultiCharacter Innocent = False
-isMultiCharacter Clear    = False
-isMultiCharacter Death    = False
-isMultiCharacter Raise    = True
-isMultiCharacter Pour     = False
-isMultiCharacter Drink    = False
-isMultiCharacter Finish   = False
+isMultiCharacter Appear    = False
+isMultiCharacter Kill      = True
+isMultiCharacter Defeat    = True
+isMultiCharacter Insult    = True
+isMultiCharacter Fight     = True
+isMultiCharacter Attack    = True
+isMultiCharacter Injure    = True
+isMultiCharacter Sentence  = True
+isMultiCharacter Bravery   = False
+isMultiCharacter Cowardice = False
+isMultiCharacter Torture   = True
+isMultiCharacter Sex       = True
+isMultiCharacter Force     = True
+isMultiCharacter Guilty    = False
+isMultiCharacter Innocent  = False
+isMultiCharacter Clear     = False
+isMultiCharacter Death     = False
+isMultiCharacter Raise     = True
+isMultiCharacter Pour      = False
+isMultiCharacter Drink     = False
+isMultiCharacter Finish    = False
 
 actionToSplitString :: Action -> (String, String)
-actionToSplitString Appear   = (" appeared in the episode ", " ")
-actionToSplitString Kill     = (" killed ", " ")
-actionToSplitString Defeat   = (" defeated ", " in combat ")
-actionToSplitString Insult   = (" insulted ", " ")
-actionToSplitString Attack   = (" attacked ", " ")
-actionToSplitString Injure   = (" injured ", " ")
-actionToSplitString Sentence = (" sentenced ", " to death ")
-actionToSplitString Torture  = (" tortured ", " ")
-actionToSplitString Sex      = (" had sex with ", " ")
-actionToSplitString Force    = (" forced sex upon ", " ")
-actionToSplitString Guilty   = (" was found guilty of a crime ", " ")
-actionToSplitString Innocent = (" was found innocent of a crime ", " ")
-actionToSplitString Clear    = (" was cleared of a wrongful charge ", " ")
-actionToSplitString Death    = (" died of natural causes ", " ")
-actionToSplitString Raise    = (" raised ", " from the dead ")
-actionToSplitString Pour     = (" poured an alcoholic drink ", " ")
-actionToSplitString Drink    = (" took an alcoholic drink ", " ")
-actionToSplitString Finish   = (" finished an alcoholic drink ", " ")
+actionToSplitString Appear    = (" appeared in the episode ", " ")
+actionToSplitString Kill      = (" killed ", " ")
+actionToSplitString Defeat    = (" defeated ", " in combat ")
+actionToSplitString Insult    = (" insulted ", " ")
+actionToSplitString Fight     = (" fight ", " ")
+actionToSplitString Attack    = (" attacked ", " ")
+actionToSplitString Injure    = (" injured ", " ")
+actionToSplitString Sentence  = (" sentenced ", " to death ")
+actionToSplitString Bravery   = (" displayed bravery ", " ")
+actionToSplitString Cowardice = (" displayed cowardice ", " ")
+actionToSplitString Torture   = (" tortured ", " ")
+actionToSplitString Sex       = (" had sex with ", " ")
+actionToSplitString Force     = (" forced sex upon ", " ")
+actionToSplitString Guilty    = (" was found guilty of a crime ", " ")
+actionToSplitString Innocent  = (" was found innocent of a crime ", " ")
+actionToSplitString Clear     = (" was cleared of a wrongful charge ", " ")
+actionToSplitString Death     = (" died of natural causes ", " ")
+actionToSplitString Raise     = (" raised ", " from the dead ")
+actionToSplitString Pour      = (" poured an alcoholic drink ", " ")
+actionToSplitString Drink     = (" took an alcoholic drink ", " ")
+actionToSplitString Finish    = (" finished an alcoholic drink ", " ")
 
 reverseActionString :: Action -> String
-reverseActionString Kill     = " killed by "
-reverseActionString Defeat   = " defeated in combat by "
-reverseActionString Insult   = " insulted by "
-reverseActionString Attack   = " attacked by "
-reverseActionString Injure   = " injured by "
-reverseActionString Sentence = " sentenced to death by "
-reverseActionString Torture  = " tortured by "
-reverseActionString Sex      = " had sex with "
-reverseActionString Force    = " was forced to have sex with "
-reverseActionString Raise    = " resurrected by "
-reverseActionString _        = error "This is not a multi-player action"
+reverseActionString Kill      = " killed by "
+reverseActionString Defeat    = " defeated in combat by "
+reverseActionString Insult    = " insulted by "
+reverseActionString Fight     = " fought "
+reverseActionString Attack    = " attacked by "
+reverseActionString Injure    = " injured by "
+reverseActionString Sentence  = " sentenced to death by "
+reverseActionString Torture   = " tortured by "
+reverseActionString Sex       = " had sex with "
+reverseActionString Force     = " was forced to have sex with "
+reverseActionString Raise     = " resurrected by "
+reverseActionString _         = error "This is not a multi-player action"
 
 weightedScoringAttributes :: Action -> (Bool, Int, Int, Int, Int)
-weightedScoringAttributes Appear   = (False,  0,   0,   0,   0)
-weightedScoringAttributes Kill     = (True,  20,  20, -20, -20)
-weightedScoringAttributes Defeat   = (True,  15,  15, -15, -15)
-weightedScoringAttributes Insult   = (True,   3,   3,  -3,  -3)
-weightedScoringAttributes Attack   = (True,   5,   5,  -5,  -5)
-weightedScoringAttributes Injure   = (True,  10,  10, -10, -10)
-weightedScoringAttributes Sentence = (True,  18,  18, -18, -18)
-weightedScoringAttributes Torture  = (True,  16,  16, -16, -16)
-weightedScoringAttributes Sex      = (True,  12,  12,  12,  12)
-weightedScoringAttributes Force    = (True,   6,   6,  -6,  -6)
-weightedScoringAttributes Guilty   = (True,  -7,  -7,   0,   0)
-weightedScoringAttributes Innocent = (True,   7,   7,   0,   0)
-weightedScoringAttributes Clear    = (True,  14,  14,   0,   0)
-weightedScoringAttributes Death    = (True,  -5,  -5,   0,   0)
-weightedScoringAttributes Raise    = (True,  20,  20,  20,  20)
-weightedScoringAttributes Pour     = (True,   1,   0,   0,   0)
-weightedScoringAttributes Drink    = (True,   2,   0,   0,   0)
-weightedScoringAttributes Finish   = (True,   4,   0,   0,   0)
+weightedScoringAttributes Appear    = (False,  0,   0,   0,   0)
+weightedScoringAttributes Kill      = (True,  20,  20, -20, -20)
+weightedScoringAttributes Defeat    = (True,  15,  15, -15, -15)
+weightedScoringAttributes Insult    = (True,   5,   5,  -5,  -5)
+weightedScoringAttributes Fight     = (True,  10,  10,  10,  10)
+weightedScoringAttributes Attack    = (True,   5,   5,  -5,  -5)
+weightedScoringAttributes Injure    = (True,  10,  10, -10, -10)
+weightedScoringAttributes Sentence  = (True,  18,  18, -18, -18)
+weightedScoringAttributes Bravery   = (True,   5,   5,   0,   0)
+weightedScoringAttributes Cowardice = (True,   5,   5,   0,   0)
+weightedScoringAttributes Torture   = (True,  16,  16, -16, -16)
+weightedScoringAttributes Sex       = (True,  12,  12,  12,  12)
+weightedScoringAttributes Force     = (True,   6,   6,  -6,  -6)
+weightedScoringAttributes Guilty    = (True,  -7,  -7,   0,   0)
+weightedScoringAttributes Innocent  = (True,   7,   7,   0,   0)
+weightedScoringAttributes Clear     = (True,  14,  14,   0,   0)
+weightedScoringAttributes Death     = (True,  -5,  -5,   0,   0)
+weightedScoringAttributes Raise     = (True,  20,  20,  20,  20)
+weightedScoringAttributes Pour      = (True,   1,   0,   0,   0)
+weightedScoringAttributes Drink     = (True,   2,   0,   0,   0)
+weightedScoringAttributes Finish    = (True,   4,   0,   0,   0)
 
 defaultScoringAttributes :: Action -> ScoringType -> (Bool, Int, Int, Int, Int)
 defaultScoringAttributes action Weighted = weightedScoringAttributes action
