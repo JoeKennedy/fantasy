abc87672ba48a984f0957d875a2ee95a07211ca9
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2016-05-05 18:03:55 -0400

    Remove extra spacing from action to string functions Add methods that capitalize first word of action to string functions

--- Model/Action.hs
+++ Model/Action.hs
@@ -2,7 +2,8 @@ module Model.Action where
 
 import Model.Types
 
-import ClassyPrelude.Yesod
+import           ClassyPrelude.Yesod
+import qualified Data.Char           as Char
 
 data Action = Appear | Kill | Defeat | Insult | Fight | Attack | Injure | Bravery | Cowardice | Sentence | Torture | Sex | Force | Guilty | Innocent | Clear | Death | Raise | Pour | Drink | Finish
     deriving (Show, Read, Eq, Enum, Bounded)
@@ -38,41 +39,49 @@ isMultiCharacter Drink     = False
 isMultiCharacter Finish    = False
 
 actionToSplitString :: Action -> (String, String)
-actionToSplitString Appear    = (" appeared in the episode ", " ")
-actionToSplitString Kill      = (" killed ", " ")
-actionToSplitString Defeat    = (" defeated ", " in combat ")
-actionToSplitString Insult    = (" insulted ", " ")
-actionToSplitString Fight     = (" fight ", " ")
-actionToSplitString Attack    = (" attacked ", " ")
-actionToSplitString Injure    = (" injured ", " ")
-actionToSplitString Sentence  = (" sentenced ", " to death ")
-actionToSplitString Bravery   = (" displayed bravery ", " ")
-actionToSplitString Cowardice = (" displayed cowardice ", " ")
-actionToSplitString Torture   = (" tortured ", " ")
-actionToSplitString Sex       = (" had sex with ", " ")
-actionToSplitString Force     = (" forced sex upon ", " ")
-actionToSplitString Guilty    = (" was found guilty of a crime ", " ")
-actionToSplitString Innocent  = (" was found innocent of a crime ", " ")
-actionToSplitString Clear     = (" was cleared of a wrongful charge ", " ")
-actionToSplitString Death     = (" died of natural causes ", " ")
-actionToSplitString Raise     = (" raised ", " from the dead ")
-actionToSplitString Pour      = (" poured an alcoholic drink ", " ")
-actionToSplitString Drink     = (" took an alcoholic drink ", " ")
-actionToSplitString Finish    = (" finished an alcoholic drink ", " ")
+actionToSplitString Appear    = ("appeared in the episode ", "")
+actionToSplitString Kill      = ("killed ", "")
+actionToSplitString Defeat    = ("defeated ", " in combat ")
+actionToSplitString Insult    = ("insulted ", "")
+actionToSplitString Fight     = ("fought ", "")
+actionToSplitString Attack    = ("attacked ", "")
+actionToSplitString Injure    = ("injured ", "")
+actionToSplitString Sentence  = ("sentenced ", " to death ")
+actionToSplitString Bravery   = ("displayed bravery ", "")
+actionToSplitString Cowardice = ("displayed cowardice ", "")
+actionToSplitString Torture   = ("tortured ", "")
+actionToSplitString Sex       = ("had sex with ", "")
+actionToSplitString Force     = ("forced sex upon ", "")
+actionToSplitString Guilty    = ("was found guilty of a crime ", "")
+actionToSplitString Innocent  = ("was found innocent of a crime ", "")
+actionToSplitString Clear     = ("was cleared of a wrongful charge ", "")
+actionToSplitString Death     = ("died of natural causes ", "")
+actionToSplitString Raise     = ("raised ", " from the dead ")
+actionToSplitString Pour      = ("poured an alcoholic drink ", "")
+actionToSplitString Drink     = ("took an alcoholic drink ", "")
+actionToSplitString Finish    = ("finished an alcoholic drink ", "")
+
+actionToSplitCaps :: Action -> (String, String)
+actionToSplitCaps action =
+    let (before, after) = actionToSplitString action
+    in  (capitalize before, after)
 
 reverseActionString :: Action -> String
-reverseActionString Kill      = " killed by "
-reverseActionString Defeat    = " defeated in combat by "
-reverseActionString Insult    = " insulted by "
-reverseActionString Fight     = " fought "
-reverseActionString Attack    = " attacked by "
-reverseActionString Injure    = " injured by "
-reverseActionString Sentence  = " sentenced to death by "
-reverseActionString Torture   = " tortured by "
-reverseActionString Sex       = " had sex with "
-reverseActionString Force     = " was forced to have sex with "
-reverseActionString Raise     = " resurrected by "
+reverseActionString Kill      = "killed by"
+reverseActionString Defeat    = "defeated in combat by"
+reverseActionString Insult    = "insulted by"
+reverseActionString Fight     = "fought"
+reverseActionString Attack    = "attacked by"
+reverseActionString Injure    = "injured by"
+reverseActionString Sentence  = "sentenced to death by"
+reverseActionString Torture   = "tortured by"
+reverseActionString Sex       = "had sex with"
+reverseActionString Force     = "was forced to have sex with"
+reverseActionString Raise     = "resurrected by"
 reverseActionString _         = error "This is not a multi-player action"
+
+reverseActionCaps :: Action -> String
+reverseActionCaps = capitalize . reverseActionString
 
 weightedScoringAttributes :: Action -> (Bool, Int, Int, Int, Int)
 weightedScoringAttributes Appear    = (False,  0,   0,   0,   0)
@@ -103,4 +112,11 @@ defaultScoringAttributes action Vanilla =
     let (isUsed, points, _, pointsRec, _) = weightedScoringAttributes action
     in  (isUsed, points, 0, pointsRec, 0)
 defaultScoringAttributes _ Scorekeeper = error "This isn't a thing"
+
+-------------
+-- Helpers --
+-------------
+capitalize :: String -> String
+capitalize []     = []
+capitalize (x:xs) = (Char.toUpper x):xs
 
