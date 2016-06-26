84fe463bca54059d70e70e6d548f96f53d9ae847
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2016-06-25 23:24:35 -0400

    Fixes #5. Email league manager when league is successfully created

--- Handler/League/ConfirmSettings.hs
+++ Handler/League/ConfirmSettings.hs
@@ -31,9 +31,9 @@ postSetupConfirmSettingsR = do
     teams <- runDB $ selectList [TeamLeagueId ==. leagueId] [Asc TeamId]
     case uncons teams of
         Nothing -> error "This league has no teams"
-        Just (leagueManagerTeam, unjoinedTeams) ->
-            -- TODO - send a congrats email to the league manager
+        Just (leagueManagerTeam, unjoinedTeams) -> do
             mapM_ (sendJoinEmail (Entity leagueId league) leagueManagerTeam) unjoinedTeams
+            sendSetupSuccessEmail (Entity leagueId league) leagueManagerTeam
     updateLeagueLastCompletedStep leagueId league 6
     redirect $ LeagueR leagueId
 
@@ -59,7 +59,7 @@ sendJoinEmail (Entity leagueId league) (Entity _ leagueManagerTeam) (Entity team
         , mailParts = return
             [ Part "text/plain" None Nothing [] $ LU.fromString $ unlines
                 [ unpack (teamOwnerName leagueManagerTeam) ++
-                    "has invited you to join an HBO's Game Of Thrones fantasy league"
+                    " has invited you to join an HBO's Game Of Thrones fantasy league "
                     ++ unpack (leagueName league)
                 , ""
                 , unpack url
@@ -69,6 +69,37 @@ sendJoinEmail (Entity leagueId league) (Entity _ leagueManagerTeam) (Entity team
     \ Thrones fantasy league called #{leagueName league}.
 <p>
     <a href="#{url}">Click here to join the league!
+|]
+            ]
+        }
+
+sendSetupSuccessEmail :: Entity League -> Entity Team -> Handler ()
+sendSetupSuccessEmail (Entity leagueId league) (Entity _ leagueManagerTeam) = do
+    master <- getYesod
+    render <- getUrlRender
+    let email = teamOwnerEmail leagueManagerTeam
+        url = render $ LeagueR leagueId
+    renderSendMailSES (appHttpManager master) (appSesCreds master email) Mail
+        { mailHeaders =
+            [ ( "Subject"
+              , "You successfully set up a fantasy league for HBO's Game Of Thrones"
+              )
+            ]
+        , mailFrom = Address Nothing "grandmaester@fantasygameofthrones.com"
+        , mailTo = [Address (Just $ teamOwnerName leagueManagerTeam) email]
+        , mailCc = []
+        , mailBcc = []
+        , mailParts = return
+            [ Part "text/plain" None Nothing [] $ LU.fromString $ unlines
+                [ "You have successfully created an HBO's Game Of Thrones fantasy league called "
+                    ++ unpack (leagueName league)
+                , ""
+                , unpack url
+                ]
+            , Part "text/html" None Nothing [] $ renderHtml [shamlet|\
+<p> You successfully created an HBO's Game Of Thrones fantasy league #
+    called <a href="#{url}">#{leagueName league}</a>.
+    Invitations to join the league have been sent to team owners.
 |]
             ]
         }
